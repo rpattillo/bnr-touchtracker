d09a614de896024554c1205db49ada55cafbc89b
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-28 18:49:54 -0500

    Implement single-touch line drawing
    DrawView supports only one touch at a time.
    As touch events are created, the touch data is used to create Line
    objects that are drawn on the view.

--- TouchTracker/DrawView.m
+++ TouchTracker/DrawView.m
@@ -7,7 +7,101 @@
 //
 
 #import "DrawView.h"
+#import "Line.h"
+
+@interface DrawView ()
+
+@property (nonatomic, strong) Line *currentLine;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
+
+
 
 @implementation DrawView
+
+#pragma mark - Initializers
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+   self = [super initWithFrame:frame];
+   
+   if (self) {
+      _finishedLines = [[NSMutableArray alloc] init];
+      self.backgroundColor = [UIColor grayColor];
+   }
+   
+   return self;
+}
+
+
+#pragma mark - Drawing
+
+- (void)strokeLine:(Line *)line
+{
+   UIBezierPath *path = [UIBezierPath bezierPath];
+   
+   path.lineWidth = 10.0;
+   path.lineCapStyle = kCGLineCapRound;
+   
+   [path moveToPoint:line.begin];
+   [path addLineToPoint:line.end];
+   
+   [path stroke];
+}
+
+
+- (void)drawRect:(CGRect)rect
+{
+   [[UIColor blackColor] set];
+   
+   for (Line *line in self.finishedLines) {
+      [self strokeLine:line];
+   }
+   
+   if (self.currentLine) {
+      [[UIColor redColor] set];
+      [self strokeLine:self.currentLine];
+   }
+}
+
+
+#pragma mark - Touch handling
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+   UITouch *touch = [touches anyObject];
+   
+   CGPoint location = [touch locationInView:self];
+   
+   self.currentLine = [[Line alloc] init];
+   
+   self.currentLine.begin = location;
+   self.currentLine.end = location;
+   
+   [self setNeedsDisplay];
+}
+
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+   UITouch *touch = [touches anyObject];
+   
+   CGPoint location = [touch locationInView:self];
+   
+   self.currentLine.end = location;
+   
+   [self setNeedsDisplay];
+}
+
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+   [self.finishedLines addObject:self.currentLine];
+   
+   self.currentLine = nil;
+   
+   [self setNeedsDisplay];
+}
 
 @end
