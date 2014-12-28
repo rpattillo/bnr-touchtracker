f097b1378bffc29e344ba7d6876ae980028dad05
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-28 17:55:08 -0500

    Remove main storyboard
    Removed the file, and set up the app delegate to prepare the UIWindow.
    It should be noted that there is no root view controller at this time.
    That will come later.  This is just a preparation step.

--- TouchTracker/AppDelegate.m
+++ TouchTracker/AppDelegate.m
@@ -15,8 +15,13 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-   // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+   
+   self.window.backgroundColor = [UIColor whiteColor];
+   [self.window makeKeyAndVisible];
+   
    return YES;
 }
 
