d0cccd711ba7a7e4919b9e30ba57a745ec4d3fa8
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-28 17:24:33 -0500

    Add XCode Single View application template
    The most basic starting point in XCode 6
    The only customization at this point is a .gitignore file

---
+++ TouchTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchTracker
+//
+//  Created by Ricky Pattillo on 12/28/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
